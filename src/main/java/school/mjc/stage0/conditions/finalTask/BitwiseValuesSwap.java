f9e866ec084c7c9d1a5d4b6cf707fc8c70402b72
Maryna Kidun <marykidun@gmail.com>
2022-10-29 23:02:28 +0300

    Finished all 6 tasks

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,11 +2,11 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public static void swap(int first, int second) {
-       int result1 = first = first ^ second;
-       int result2 = second = first ^ second;
-       result1 = first = first ^ second;
-        System.out.println(result1);
-        System.out.println(result2);
+       first = first ^ second;
+       second = first ^ second;
+       first = first ^ second;
+        System.out.println(first);
+        System.out.println(second);
     }
     public static void main(String[] args) {
         swap(5, 9);
